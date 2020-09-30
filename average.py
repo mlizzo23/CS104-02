b0908b54fbd5211018b6e8a422d5cc5fcb4c08c2
mlizzo23 <71794417+mlizzo23@users.noreply.github.com>
2020-09-30 18:20:58 -0400

    Add files via upload
    To submit for oct 5

--- average.py
+++ average.py
@@ -9,10 +9,14 @@ average = 0.0
 numberOfScores = int (input("Please enter the number of scores you want to average: ") )
 
 #Add a loop to make this code repeat until scorecount = numberOfScores
-score = int (input ("Please enter a score: ") )
-total = total + score
-scoreCount = scoreCount + 1
+
+while scoreCount < numberOfScores:
+    score = int (input ("Please enter a score: ") )
+    total = total + score
+    scoreCount = scoreCount + 1
 
 
 average = total / numberOfScores
-print (average) 
+print
+print (average) #print
+
