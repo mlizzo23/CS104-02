e547f41ceb1d1e135d51477e0d90c92d987ba6cc
mlizzo23 <71794417+mlizzo23@users.noreply.github.com>
2020-10-05 17:41:09 -0400

    Add files via upload
    conditions.py

---
+++ conditions.py
@@ -0,0 +1,27 @@
+a = 90
+b = 70 
+c = 50
+d = 32
+
+temp = int(input("Please enter the current temperature"))  
+
+while temp != 999:
+        temp = int(input("Please enter the current temperature"))  
+        if temp > a:
+            print ("Wear Shorts")
+        elif temp >= b:
+            print ("Short sleeves are fine")
+        elif temp >= c:
+            print ("Wear a jacket")
+        elif temp >= d:
+            print ("Wear a heavy coat")
+        
+
+
+
+
+# do while (user input != 999){
+     
+
+
+    
