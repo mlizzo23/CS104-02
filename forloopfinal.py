0ec12878947f8bae49681b3a9ee2b589fff67264
mlizzo23 <71794417+mlizzo23@users.noreply.github.com>
2020-10-26 17:45:59 -0400

    Add files via upload

---
+++ forloopfinal.py
@@ -0,0 +1,21 @@
+#--------------------------------------------------------------------
+#counting loop using while
+#print ("counting loop using while")
+#x=1
+total =0
+#while x <= 10:
+ #   print("x= ",x)
+ #   x = x+1
+#print()
+#--------------------------------------------------------------------
+#--------------------------------------------------------------------
+#simple counting loop with range
+
+print ("simple counting loop with range")
+for x in range (0,10):
+    total = total +x
+    #print ("x= ",x)
+    #print ("total=", total)
+print ()
+print ("total=", total)
+#--------------------------------------------------------------------
